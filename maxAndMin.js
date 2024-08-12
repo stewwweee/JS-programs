91ab597d9b164be91d04cf42b53742c5b75c8880
stewwweee <chrisstewartsocialmedia@gmail.com>
2024-08-12 22:42:30 +0530

    Write a program to find Max and Min number

---
+++ maxAndMin.js
@@ -0,0 +1,14 @@
+let arr = [5,8,3,7,9,23,85,-24];
+let min = arr[0];
+let max = arr[0];
+
+for (let i=1;i<arr.length;i++){
+    if (arr[i] < min){
+        min = arr[i];
+    }
+    if (arr[i] > max){
+        max = arr[i]
+    }
+}
+    console.log(min);
+    console.log(max);
