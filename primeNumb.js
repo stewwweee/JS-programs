72ce71c43e40cbdd0716987d35e72edb51be77f5
stewwweee <chrisstewartsocialmedia@gmail.com>
2024-08-19 20:21:25 +0530

    write a js program to print prime numbers from 1 to 100

---
+++ primeNumb.js
@@ -0,0 +1,16 @@
+let primes = [];
+
+for (let i = 2; i <= 100; i++) {
+    let isPrime = true;
+    for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        primes.push(i);
+    }
+}
+
+console.log(primes);
