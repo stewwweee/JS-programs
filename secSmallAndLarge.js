df995621ef99c35e957f6cab1870b15011c73de7
stewwweee <chrisstewartsocialmedia@gmail.com>
2025-08-07 08:24:31 +0530

    seclarge and secmin

--- secSmallAndLarge.js
+++ secSmallAndLarge.js
@@ -1,21 +1,29 @@
-let arr = [5,8,3,7,9,23,85,-24];
+let arr = [5, 8, 3, 7, 9, 23, 85, -24, 16, 94, 93, -25];
+
 let min = arr[0];
 let max = arr[0];
 let secLarge = arr[0];
 let secSmall = arr[0];
 
-for (let i=0;i<arr.length;i++){
-  
-    if (arr[i] > max){
-        secLarge = max; // Update second largest before changing max
-        max = arr[i]
+for (let i = 0; i < arr.length; i++) {
+    // Update max and second largest
+    if (arr[i] > max) {
+        secLarge = max;
+        max = arr[i];
+    } else if (arr[i] > secLarge && arr[i] !== max) {
+        secLarge = arr[i];
     }
-    if (arr[i] < min){
-        secSmall = min; // Update second smallest before changing min
+
+    // Update min and second smallest
+    if (arr[i] < min) {
+        secSmall = min;
         min = arr[i];
+    } else if (arr[i] < secSmall && arr[i] !== min) {
+        secSmall = arr[i];
     }
 }
 
+console.log("Second Largest:", secLarge);
+console.log("Second Smallest:", secSmall);
 
-console.log(secLarge);
-console.log(secSmall);
+
